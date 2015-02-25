9ec0f60f55726532dd5804f11f589798d598cd81
Jackson Henry <jackson.henry3@gmail.com>
2015-02-25 15:29:50 -0500

    fixed weighting

--- attempt1.py
+++ attempt1.py
@@ -1,4 +1,3 @@
-import numpy as np
 import re
 import random as r
 
@@ -8,9 +7,10 @@ novel = open(file,"r")
 text  = novel.read()
 
 wordArr = [w for w in re.split('\W',text) if w]
+
+
 word_Graph = {}
 
-a = {}
 for i in range(len(wordArr)-1):
 	word      = wordArr[i]
 	next_Word = wordArr[i+1]
@@ -19,11 +19,14 @@ for i in range(len(wordArr)-1):
 	elif word in word_Graph:
 		if next_Word not in word_Graph[word]:
 			word_Graph[word][next_Word] = 1.
-		if next_Word in word_Graph[word]:
+		elif next_Word in word_Graph[word]:
 			word_Graph[word][next_Word] += 1.
+
+
 
 seed = wordArr[int(r.random()*len(wordArr))]
 sentance = seed
+word = seed
 for i in range(100):
 	rand = r.random()
 	total = sum(word_Graph[word].values())
@@ -31,7 +34,10 @@ for i in range(100):
 	for next_word in word_Graph[word]:
 		a+=word_Graph[word][next_word]/total
 		if rand<a:
+			pword = word
 			word = next_word
 			break
 	sentance+=' '+next_word
+# 	print(pword,word_Graph[pword])
 print sentance
+
